ac3a7cab859c2e55cfdd5bdd2a20d546687eb0cd
jgowtham8 <jgowthaman1308@gmail.com>
2023-09-03 21:29:11 +0530

    ui

--- lib/models/general.dart
+++ lib/models/general.dart
@@ -1,5 +1,58 @@
-class SalesData {
-  SalesData(this.year, this.sales);
-  final String year;
-  final double sales;
+class EnergyData {
+  EnergyData(this.month, this.units);
+
+  final String month;
+  final double units;
+}
+
+class Device {
+  String name;
+  double soFarConsumedEnergyInKwh;
+  bool isON;
+
+  Device({
+    required this.name,
+    required this.soFarConsumedEnergyInKwh,
+    required this.isON,
+  });
+
+  static List<Device> getDeviceList() {
+    return [
+      Device(
+        name: 'Refrigerator',
+        soFarConsumedEnergyInKwh: 42.23,
+        isON: true,
+      ),
+      Device(
+        name: 'Washing machine',
+        soFarConsumedEnergyInKwh: 20.6,
+        isON: true,
+      ),
+      Device(
+        name: 'TV - Bedroom',
+        soFarConsumedEnergyInKwh: 12.36,
+        isON: true,
+      ),
+      Device(
+        name: 'Light - Hall',
+        soFarConsumedEnergyInKwh: 5.3,
+        isON: true,
+      ),
+      Device(
+        name: 'TV - Hall',
+        soFarConsumedEnergyInKwh: 18.5,
+        isON: false,
+      ),
+      Device(
+        name: 'Light - Bedroom',
+        soFarConsumedEnergyInKwh: 4.66,
+        isON: false,
+      ),
+      Device(
+        name: 'Air conditioner',
+        soFarConsumedEnergyInKwh: 90.36,
+        isON: true,
+      ),
+    ];
+  }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,18 +12,10 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   // Define the index of the currently selected item
   int _currentIndex = 0;
-
-  // Define the list of pages to show according to the selected item
-  List<Widget> _pages = [];
+  final List<Device> _deviceList = Device.getDeviceList();
 
   @override
   void initState() {
-    _pages = [
-      _getHome(),
-      const Center(child: Text('Search')),
-      const Center(child: Text('Profile')),
-    ];
-
     super.initState();
   }
 
@@ -33,26 +25,69 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  Widget _getNow() {
+    return ListView.builder(
+      itemCount: _deviceList.length,
+      itemBuilder: (context, index) {
+        final device = _deviceList[index];
+        return Container(
+          margin: const EdgeInsets.only(right: 10, left: 10, top: 10),
+          decoration: BoxDecoration(
+            color: (device.isON)
+                ? Colors.grey.withOpacity(0.8)
+                : Colors.grey.withOpacity(0.4),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(10),
+            ),
+          ),
+          child: ListTile(
+            title: Text(
+              device.name,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+              ),
+            ),
+            subtitle: Text(
+              '${device.soFarConsumedEnergyInKwh} Kwh consumed',
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.7),
+                fontSize: 12,
+              ),
+            ),
+            trailing: Switch(
+              value: _deviceList[index].isON,
+              onChanged: (value) {
+                setState(() {
+                  _deviceList[index].isON = value;
+                });
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   Widget _getHome() {
     return Center(
-      child: Container(
-        child: SfCartesianChart(
-          // Initialize category axis
-          primaryXAxis: CategoryAxis(),
-          series: <LineSeries<SalesData, String>>[
-            LineSeries<SalesData, String>(
-                // Bind data source
-                dataSource: <SalesData>[
-                  SalesData('Jan', 35),
-                  SalesData('Feb', 28),
-                  SalesData('Mar', 34),
-                  SalesData('Apr', 32),
-                  SalesData('May', 40)
-                ],
-                xValueMapper: (SalesData sales, _) => sales.year,
-                yValueMapper: (SalesData sales, _) => sales.sales)
-          ],
-        ),
+      child: SfCartesianChart(
+        // Initialize category axis
+        primaryXAxis: CategoryAxis(),
+        series: <LineSeries<EnergyData, String>>[
+          LineSeries<EnergyData, String>(
+            // Bind data source
+            dataSource: <EnergyData>[
+              EnergyData('Jan', 35),
+              EnergyData('Feb', 28),
+              EnergyData('Mar', 34),
+              EnergyData('Apr', 32),
+              EnergyData('May', 40)
+            ],
+            xValueMapper: (EnergyData sales, _) => sales.month,
+            yValueMapper: (EnergyData sales, _) => sales.units,
+          )
+        ],
       ),
     );
   }
@@ -61,15 +96,30 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter Bottom Navigation Bar Example'),
+        title: const Text('Energy Manager'),
+        centerTitle: true,
+        elevation: 1,
       ),
-      body: _pages[_currentIndex], // Show the page according to the index
+      body: [
+        _getNow(),
+        _getHome(),
+        const Center(child: Text('Search')),
+        const Center(child: Text('Profile')),
+      ].elementAt(_currentIndex), // Show the page according to the index
       bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.deepPurple,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
         items: const [
           // Define the items for the bottom navigation bar
           BottomNavigationBarItem(
+            icon: Icon(Icons.equalizer),
+            label: 'Now',
+          ),
+          BottomNavigationBarItem(
             icon: Icon(Icons.home),
-            label: 'Home',
+            label: 'Usage',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.search),
